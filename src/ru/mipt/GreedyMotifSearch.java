00392d98551fb093d28ef6f63c876d2d073fd7de
Nikitos-U <uhov.na@phystech.edu>
2019-10-07 05:54:34 +0300

    Working

--- src/ru/mipt/GreedyMotifSearch.java
+++ src/ru/mipt/GreedyMotifSearch.java
@@ -3,67 +3,76 @@ package ru.mipt;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class GreedyMotifSearch {
     public static int a = 0;
-    public static String[] greedyMotifSearch(String[] dna, int k, int t) {
-        String[] bestMotif = new String[dna.length];
+
+    public static List<String> greedyMotifSearch(String[] dna, int k, int t) {
+        List<String> bestMotif = new ArrayList<>();
+        List<String> toProfile = new ArrayList<>();
         for (int i = 0; i < t; i++) {
-            bestMotif[i] = dna[i].substring(0, k);
+            bestMotif.add(i, dna[i].substring(0, k));
         }
-        String[] toProfile = new String[t];
         for (int i = 0; i < dna[0].length() - k + 1; i++) {
-            toProfile[0] = dna[0].substring(i, i + k);
+            toProfile.clear();
+            toProfile.add(0, dna[0].substring(i, i + k));
             for (int j = 1; j < t; j++) {
-                a = j;
-                toProfile[j] = mostProbableKMer(dna[j], k, profile(toProfile));
-                System.out.println("rabotau");
+                toProfile.add(j, mostProbableKMer(dna[j], k, profile(toProfile, k)));
             }
-            if (score(toProfile) > score(bestMotif)) {
-                bestMotif = toProfile;
+            System.out.println(bestMotif);
+            int d = score(bestMotif, k);
+            int lokh = score(toProfile, k);
+            if (d > lokh) {
+                bestMotif.clear();
+                for (String s : toProfile) {
+                    bestMotif.add(s);
+                }
+                System.out.println("%%%%%%%%%%%%%");
+                System.out.println(bestMotif);
+                System.out.println(toProfile);
+                System.out.println("%%%%%%%%%%%%%%");
+
             }
         }
         return (bestMotif);
     }
 
-    public static double[][] profile(String[] motifs) {
-        float n = motifs.length;
-        double[][] profile = new double[4][motifs[0].length()];
+    public static double[][] profile(List<String> motifs, int k) {
+        float n = motifs.size();
+        double[][] profile = new double[4][k];
         int[] counter = new int[4];
-        for (int i = 0; i < motifs[0].length(); i++) {
-            for (int k = 0; k < counter.length; k++) {
-                counter[k] = 0;
+        for (int i = 0; i < k; i++) {
+            for (int z = 0; z < counter.length; z++) {
+                counter[z] = 0;
             }
-            for (int j = 0; j < a; j++) {
-                switch (motifs[j].charAt(i)) {
+            for (int j = 0; j < motifs.size(); j++) {
+                switch (motifs.get(j).charAt(i)) {
                     case 'A':
                         counter[0]++;
-                        profile[0][i] = counter[0] / a;
+                        profile[0][i] = counter[0] / n;
                         break;
                     case 'G':
                         counter[1]++;
-                        profile[1][i] = counter[1] / a;
+                        profile[1][i] = counter[1] / n;
                         break;
                     case 'T':
                         counter[2]++;
-                        profile[2][i] = counter[2] / a;
+                        profile[2][i] = counter[2] / n;
                         break;
                     case 'C':
                         counter[3]++;
-                        profile[3][i] = counter[3] / a;
+                        profile[3][i] = counter[3] / n;
                         break;
                 }
             }
         }
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < motifs[0].length(); j++) {
-                System.out.print(profile[i][j] + " ");
-            }
-            System.out.println();
-        }
+        //       for (int i = 0; i < 4; i++) {
+        //           for (int j = 0; j < k; j++) {
+        //               System.out.print(profile[i][j] + " ");
+        //           }
+//            System.out.println();
+        //       }
         return (profile);
     }
 
@@ -89,7 +98,7 @@ public class GreedyMotifSearch {
                         break;
                 }
             }
-            if (max < points) {
+            if (max < points || mostProbableKMer == null) {
                 max = points;
                 mostProbableKMer = kMer;
             }
@@ -97,13 +106,16 @@ public class GreedyMotifSearch {
         return (mostProbableKMer);
     }
 
-    public static int score(String[] toProfile) {
+    public static int score(List<String> toProfile, int k) {
         int counter = 0;
-        int score = 0;
+        int score1 = 0;
         int[] counter1 = new int[4];
-        for (int i = 0; i < toProfile[0].length(); i++) {
-            for (int j = 0; j < toProfile.length; j++) {
-                switch (toProfile[i].charAt(j)) {
+        for (int i = 0; i < k; i++) {
+            for (int l = 0; l < counter1.length; l++) {
+                counter1[l] = 0;
+            }
+            for (int j = 0; j < toProfile.size(); j++) {
+                switch (toProfile.get(j).charAt(i)) {
                     case 'A':
                         counter1[0]++;
                         break;
@@ -118,27 +130,29 @@ public class GreedyMotifSearch {
                         break;
                 }
             }
-            counter = toProfile[i].length() - Math.max(Math.max(counter1[0], counter1[1]), Math.max(counter1[2], counter1[3]));
-            score += counter;
+            //    System.out.println(toProfile.size()+ "-" + Math.max(Math.max(counter1[0], counter1[1]), Math.max(counter1[2], counter1[3])));
+            counter = toProfile.size() - Math.max(Math.max(counter1[0], counter1[1]), Math.max(counter1[2], counter1[3]));
+            score1 += counter;
+            //       System.out.println(score);
         }
-        return (score);
+        return (score1);
     }
 
     public static void main(String[] args) throws IOException {
-            Scanner scanner = new Scanner(System.in);
-            String Filename = scanner.nextLine();
-            int k = scanner.nextInt();
-            int t = scanner.nextInt();
-            FileReader stream = new FileReader(Filename);
-            BufferedReader input = new BufferedReader(stream);
-            String[] dna = new String[t];
-            for(int i = 0; i < t; i++) {
-                dna[i] = input.readLine();
-            }
-        String[] result;
+        Scanner scanner = new Scanner(System.in);
+        String Filename = scanner.nextLine();
+        int k = scanner.nextInt();
+        int t = scanner.nextInt();
+        FileReader stream = new FileReader(Filename);
+        BufferedReader input = new BufferedReader(stream);
+        String[] dna = new String[t];
+        for (int i = 0; i < t; i++) {
+            dna[i] = input.readLine();
+        }
+        List<String> result;
         result = greedyMotifSearch(dna, k, t);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
         }
     }
 }
